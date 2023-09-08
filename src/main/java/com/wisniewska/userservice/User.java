cb6b3eefc73715d98c547698174b93c53daa322b
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-09-08 22:33:40 +0200

    Update User

--- src/main/java/com/wisniewska/userservice/User.java
+++ src/main/java/com/wisniewska/userservice/User.java
@@ -1,6 +1,19 @@
 package com.wisniewska.userservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
 
   private String userId;
+  private String userName;
+  private String userPhoneNumber;
+
+
+
 }
